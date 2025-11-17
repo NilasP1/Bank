b2732142c997da011ca38272e8b848dd588ac8f7
NilasP1 <nilas.petersson@gmail.com>
2025-11-17 14:31:06 +0100

    Added comments to the User class

--- Bank/User.cs
+++ Bank/User.cs
@@ -9,11 +9,18 @@ namespace Bank
 {
     internal class User
     {
-        public string username;
-        public string password;
+        public string username; // username of the user
+        public string password; // password of the user
 
-        public List<Account> accountsOnThisUser = new List<Account>();
+        public List<Account> accountsOnThisUser = new List<Account>(); // list of accounts associated with this user
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class with the specified username and password.
+        /// </summary>
+        /// <remarks>Ensure that the provided username and password meet any application-specific
+        /// validation requirements before creating a new instance of the <see cref="User"/> class.</remarks>
+        /// <param name="usercreatename">The username to associate with the user. Cannot be null or empty.</param>
+        /// <param name="passwordcreate">The password to associate with the user. Cannot be null or empty.</param>
         public User(string usercreatename, string passwordcreate)
         {
             this.username = usercreatename;

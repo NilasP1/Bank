5e4a3c0cabeed90a0725cf7839442176f1144166
NilasP1 <nilas.petersson@gmail.com>
2025-11-17 14:30:00 +0100

    Added comments to Account class

--- Bank/Account.cs
+++ Bank/Account.cs
@@ -8,12 +8,17 @@ namespace Bank
 {
     internal class Account
     {
-        public float balance;
+        public float balance; 
         public string name;
 
-        public List<string> transactionHistory = new List<string>();
+        public List<string> transactionHistory = new List<string>(); // List to store transaction history
 
-        public Account(string name, float balance)
+        /// <summary>
+        /// constructor for account class
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="balance"></param>
+        public Account(string name, float balance) 
         {
             this.balance = balance;
             this.name = name;
@@ -22,6 +27,12 @@ namespace Bank
             
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Account"/> class with a default balance of zero and a default
+        /// name.
+        /// </summary>
+        /// <remarks>The account is initialized with a balance of 0 and the name "No name on the account".
+        /// A transaction history entry is added to indicate the account creation.</remarks>
         public Account()
         {
             balance = 0;
@@ -31,6 +42,12 @@ namespace Bank
 
         }
 
+        /// <summary>
+        /// Deposits the specified amount into the account.
+        /// </summary>
+        /// <remarks>Updates the account balance and records the transaction in the transaction
+        /// history.</remarks>
+        /// <param name="amount">The amount to deposit. Must be a positive value.</param>
         public void deposit(float amount)
         {
             balance += amount;
